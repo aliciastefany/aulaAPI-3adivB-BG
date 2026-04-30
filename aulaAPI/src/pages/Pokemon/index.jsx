31d6ed4daf097db275a4b3cff86c111a929043fe
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-04-30 20:33:56 +0000

    update final

--- aulaAPI/src/pages/Pokemon/index.jsx
+++ aulaAPI/src/pages/Pokemon/index.jsx
@@ -1,8 +1,62 @@
+import { useState } from "react"
+import "./style.css"
+
+const pokemonList =[
+    {id:1, nome:'Bulbassauro'},
+    {id:4, nome:'Charmander'},
+    {id:6, nome:'Charizard'},
+    {id:7, nome:'Squirtle'},
+    {id:152, nome:'Chikorita'}
+]
+
 function Pokemon(){
+    const [pokemonGlobal, setPokemonGlobal] = useState(null)
+
+    const getPokemonData = (idPokemon)=>{
+        const uri = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`
+        
+        fetch(uri)
+            .then(res=>res.json())
+                .then(json=>{
+                    const pokemonFetch = {
+                        nome: json.name,
+                        peso: json.weight,
+                        vida: json.stats[0].base_stat,
+                        imagem: json.sprites.other['official-artwork'].front_default
+                    }
+                    setPokemonGlobal(pokemonFetch)
+                    console.log(pokemonFetch)
+                })
+                .catch(()=>alert("Não foi possível acessar os dados do Pokémon"))
+    }
+
+
     return(
-        <div>
-            <h1>Página de pokémon</h1>
-            <p>Faça a sua escolha!</p>
+        <div className="container">
+            <div className="header">
+                <h1>Lista de pokémon</h1>
+                <p>Faça a sua escolha!</p>
+            </div>
+
+            {pokemonGlobal &&(
+                <div className="pokemon-info">
+                    <h2>Nome: {pokemonGlobal.nome}</h2>
+                    <p>Peso: {pokemonGlobal.peso}</p>
+                    <p>Vida: {pokemonGlobal.vida}</p>
+                    <img
+                        src={pokemonGlobal.imagem}
+                        alt={pokemonGlobal.nome}
+                    />
+                </div>
+            )}
+
+            {pokemonList.map((item)=>(
+                <div className="card" key={item.id}>
+                    <p>{item.nome}</p>
+                    <button onClick={()=>getPokemonData(item.id)}>Saiba mais</button>
+                </div>
+            ))}
+
         </div>
     )
 }
